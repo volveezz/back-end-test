b9ca65ec880e28a3cbe0be3a61279642905f3401
Volve <beconich@gmail.com>
2025-01-13 15:07:33 +0100

    feat: add secure cookie option based on environment variable and update example env file

--- src/delivery/http/v1/handlers/auth/authorize.ts
+++ src/delivery/http/v1/handlers/auth/authorize.ts
@@ -15,7 +15,7 @@ export const buildAuthorize = ({ auth }: Params): Authorize => {
     // Save the access token in user cookies
     res.cookie('accessToken', data.accessToken, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.ENABLE_SECURE_COOKIES === 'true' ? true : false,
       sameSite: 'strict',
       // Expires in 24 hours
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
